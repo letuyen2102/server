9f2fbdfa917ff965aab7027b3cf39b97c0c70aae
letuyen <letuyenkhtn212@gmail.com>
2023-05-22 23:33:54 +0700

    ....

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.use(express.static('./public/image'));
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/' , (req,res) => {
+    res.status(200).json({
+        status : 'success',
+        message : "hello"
+    })
+})
 app.use('/myway/api/products', productRouter)
 app.use('/myway/api/users' , userRouter)
 
